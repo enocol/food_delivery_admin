fb4499b8375fa03c06a7c06be9a18304983cb62c
enocol <eno_col@yahoo.com>
2026-04-19 09:09:39 +0100

    add background image

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import '../App.css'
+import homeBg from '../assets/home-bg.svg'
 
 const Home = () => {
   return (
-    <div className="home-page">
+    <div className="home-page" style={{ backgroundImage: `url(${homeBg})` }}>
       <header className="home-header">
         <h1>MBOLO EATS DASHBOARD</h1>
       </header>
